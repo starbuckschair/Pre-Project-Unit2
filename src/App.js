f862a3ab581efd3f3cbef4a2f648e5830c7f7d9d
starbuckschair <water1793@gmail.com>
2022-08-22 15:57:08 +0900

    과제제출2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and saveㅁㄴㄹㅇㄴㄹㅁㄹ to reload.
+          Edit <code>src/App.js</code> and save과제제출 to reload.
         </p>
         <a
           className="App-link"
